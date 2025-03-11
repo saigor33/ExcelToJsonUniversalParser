32e7e65aa95a30df5640617344e39994699c238d
saigor33 <saigor33@yandex.ru>
2025-03-11 22:54:49 +0300

    Refactor: Add print missing ref value after shet names ended

--- Parser2/PythonScripts/RowToJsonConverter/RefNodesJoiner.py
+++ Parser2/PythonScripts/RowToJsonConverter/RefNodesJoiner.py
@@ -42,26 +42,36 @@ class RefNodesJoiner:
     def _GetRefValue(self, node: Node, nodes_layer: NodesLayer) -> list[Node]:
         if not self._IsRefNode(node):
             raise Exception("Node is no reference node")
-        if nodes_layer is None:
-            raise Exception("NodesLayer can't be NoNE")
 
         ref_node_field_name = node.inner_nodes[0].value
 
-        for next_layer_node in nodes_layer.nodes:
-            if next_layer_node.name == ref_node_field_name:
-                for index, next_layer_inner_node in enumerate(next_layer_node.inner_nodes):
-                    next_layer_node.inner_nodes[index] = self._JoinInternal(next_layer_inner_node,
-                                                                            nodes_layer.next_nodes_layer)
-                return next_layer_node.inner_nodes
+        if nodes_layer is not None:
+            for next_layer_node in nodes_layer.nodes:
+                if next_layer_node.name == ref_node_field_name:
+                    for index, next_layer_inner_node in enumerate(next_layer_node.inner_nodes):
+                        next_layer_node.inner_nodes[index] = self._JoinInternal(next_layer_inner_node,
+                                                                                nodes_layer.next_nodes_layer)
+                    return next_layer_node.inner_nodes
 
         print(Fore.RED + 'Error: Missing ref value' + Style.RESET_ALL)
 
         table = PrettyTable()
-        table.field_names = ["Search sheet name", "id"]
-        highlighted_ref_node_field_name = Fore.RED + ref_node_field_name + Style.RESET_ALL
-        table.add_row([nodes_layer.sheet_name, highlighted_ref_node_field_name])
+        table.field_names = ['Search sheet name', 'id']
+
+        description: str
+        missing_id: str
+        if nodes_layer is None:
+            description = 'Sheet names ended'
+            sheet_name = Fore.RED + '<Sheet names ended>' + Style.RESET_ALL
+            missing_id = ref_node_field_name
+        else:
+            description = 'Unknown "id"'
+            sheet_name = nodes_layer.sheet_name
+            missing_id = Fore.RED + ref_node_field_name + Style.RESET_ALL
+
+        table.add_row([sheet_name, missing_id])
         error: list[str] = [
-            '\n\tDescription: Unknown "id"',
+            '\n\tDescription: ' + description,
             '\n' + str(table),
             '\n'
         ]
