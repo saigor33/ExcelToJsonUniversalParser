5bfed41e591327736e0d489d3374910aa0e3eda8
saigor33 <saigor33@yandex.ru>
2025-05-10 13:44:38 +0300

    Added feature: Replaced option parameters in parse config

--- Parser2/PythonScripts/Sources/Configuration/ConfigLoader.py
+++ Parser2/PythonScripts/Sources/Configuration/ConfigLoader.py
@@ -1,4 +1,6 @@
 import json
+from typing import Optional
+from Sources.Configuration import DefaultValues
 from Sources.Configuration.Configs.Config import Config
 from Sources.Configuration.Configs.DebugConfig import DebugConfig
 from Sources.Configuration.Configs.ExcelSourceConfig import ExcelSourceConfig
@@ -15,11 +17,18 @@ class ConfigLoader:
     def Load(self) -> Config:
         config_json = self.__LoadJson()
 
-        padding_per_layer: str = config_json['paddingPerLayer']
-        debug_config: DebugConfig = self.__LoadDebugConfig(config_json['debug'])
-        selected_source_type: SourceType = self.__LoadSourceType(config_json['selectedSourceType'])
-        excel_source_config = self.__LoadExcelSourceConfig(config_json['excelSource'])
-        google_sheets_source_config = self.__LoadGoogleSheetsSourceConfig(config_json['googleSheetsSource'])
+        padding_per_layer: str = self.__LoadPaddingPerLayer(config_json.get('paddingPerLayer'))
+        debug_config: DebugConfig = self.__LoadDebugConfig(config_json.get('debug'))
+
+        selected_source_type: SourceType = self.__LoadSourceType(config_json.get('selectedSourceType'))
+        excel_source_config: Optional[ExcelSourceConfig] = None
+        google_sheets_source_config: Optional[GoogleSheetsSourceConfig] = None
+        if selected_source_type == SourceType.Excel:
+            excel_source_config = self.__LoadExcelSourceConfig(config_json.get('excelSource'))
+        elif selected_source_type == SourceType.GoogleSheet:
+            google_sheets_source_config = self.__LoadGoogleSheetsSourceConfig(config_json.get('googleSheetsSource'))
+        else:
+            raise Exception(f"Unknown source type '{selected_source_type}'")
 
         parsing_feature_configs: dict[str, ParsingFeatureConfig] = self.__LoadParsingFeatureConfigs(config_json)
 
@@ -28,13 +37,19 @@ class ConfigLoader:
 
     def __LoadJson(self):
         json_file = open(self.__config_file_path, "r")
-
         config_json = json.loads(json_file.read())
         json_file.close()
 
         return config_json
 
+    @staticmethod
+    def __LoadPaddingPerLayer(json_value):
+        return json_value if json_value is not None else DefaultValues.PaddingPerLayer
+
     def __LoadExcelSourceConfig(self, config_json) -> ExcelSourceConfig:
+        if config_json is None:
+            raise Exception("Excel source json config not filled")
+
         excel_file_path: str = config_json['excelFilePath']
         features_parsing_config: ParsingConfig = self.__LoadParsingConfig(config_json['featuresParsing'])
         alias_funcs_parsing_config: ParsingConfig = self.__LoadParsingConfig(config_json['aliasFuncsParsing'])
@@ -42,6 +57,9 @@ class ConfigLoader:
         return ExcelSourceConfig(excel_file_path, features_parsing_config, alias_funcs_parsing_config)
 
     def __LoadGoogleSheetsSourceConfig(self, config_json) -> GoogleSheetsSourceConfig:
+        if config_json is None:
+            raise Exception("GoogleSheets source json config not filled")
+
         credentials_file_path: str = config_json['credentialsFilePath']
         spreadsheet_id: str = config_json['spreadsheetId']
         features_parsing_config: ParsingConfig = self.__LoadParsingConfig(config_json['featuresParsing'])
@@ -57,24 +75,31 @@ class ConfigLoader:
         if str_source_type == 'Excel':
             return SourceType.Excel
 
-        raise ValueError(f'Unknown source type {str_source_type}')
+        raise ValueError(f"selectedSourceType' type not filled or supported: '{str_source_type}'")
 
     @staticmethod
     def __LoadDebugConfig(config_json) -> DebugConfig:
-        need_print_benchmarks = bool(config_json["needPrintBenchmarks"])
+        if config_json is None:
+            return DefaultValues.Debug
+
+        need_print_benchmarks = bool(config_json.get("needPrintBenchmarks", False))
         return DebugConfig(need_print_benchmarks)
 
     @staticmethod
     def __LoadParsingFeatureConfigs(config_json) -> dict[str, ParsingFeatureConfig]:
+        parsing_features_json = config_json.get('parsingFeatures')
+        if parsing_features_json is None:
+            raise Exception("'parsingFeatures' not filled")
+
         result: dict[str, ParsingFeatureConfig] = {}
 
-        for parsing_feature_json in config_json['parsingFeatures']:
+        for parsing_feature_json in parsing_features_json:
             feature_name: str = parsing_feature_json['featureName']
             output_directory: str = parsing_feature_json['outputDirectory']
             output_file_name: str = parsing_feature_json['outputFileName']
 
             if feature_name in result:
-                raise Exception(''.join(["Feature already added (", feature_name, ")"]))
+                raise Exception(f"Feature already added '{feature_name}'")
 
             result[feature_name] = ParsingFeatureConfig(output_directory, output_file_name)
 
---
+++ Parser2/PythonScripts/Sources/Configuration/DefaultValues.py
@@ -0,0 +1,4 @@
+from Sources.Configuration.Configs.DebugConfig import DebugConfig
+
+PaddingPerLayer: str = "    "
+Debug: DebugConfig = DebugConfig(need_print_benchmarks=False)
