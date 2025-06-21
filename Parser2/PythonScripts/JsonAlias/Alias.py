8458cdb9c7945b8c3e122e9eb5d2f97dbb29b496
saigor33 <saigor33@yandex.ru>
2025-06-21 17:04:46 +0300

    Added feature: Arg type in json alias func

--- Parser2/PythonScripts/JsonAlias/Alias.py
+++ Parser2/PythonScripts/JsonAlias/Alias.py
@@ -1,5 +1,8 @@
+from prettytable import PrettyTable
 from AliasFuncs import Logger
 from typing import Optional
+from RowToJsonConverter import AliasFuncStackLogFormatter
+from Tests import LogFormatter
 
 
 class Item:
@@ -12,8 +15,75 @@ class TextItem(Item):
 
 
 class ArgItem(Item):
-    def __init__(self, arg_name: str):
+    class ValueType:
+        String = 'str'
+        Number = 'num'
+        Bool = 'bool'
+        # Null can be set in alias func jsom.
+
+    def __init__(self, arg_name: str, arg_type: str):
         self.arg_name = arg_name
+        self._arg_type = arg_type
+
+    def convertValue(
+            self,
+            arg_value: str,
+            alias_func_stack: list[str],
+            current_alias_func_name: str,
+            root_field_names_stack: list[str],
+            current_root_field_name: str
+    ) -> str:
+        if self._arg_type == ArgItem.ValueType.String:
+            return arg_value
+        elif self._arg_type == ArgItem.ValueType.Number:
+            return arg_value
+        elif self._arg_type == ArgItem.ValueType.Bool:
+            bool_value = arg_value.lower()
+            if bool_value == 'true' or bool_value == '1' or bool_value == '1.0':
+                return "true"
+            if bool_value == 'false' or bool_value == '0' or bool_value == '0.0':
+                return "false"
+
+            self._LogInvalidValue(
+                arg_value,
+                alias_func_stack,
+                current_alias_func_name,
+                root_field_names_stack,
+                current_root_field_name
+            )
+            return arg_value
+        else:
+            raise Exception(f'Unsupported arg type for json alias func. (arg_type:{self._arg_type})')
+
+    def _LogInvalidValue(
+            self,
+            arg_value: str,
+            alias_func_stack: list[str],
+            current_alias_func_name: str,
+            root_field_names_stack: list[str],
+            current_root_field_name: str
+    ):
+        pretty_table = PrettyTable()
+        pretty_table.field_names = ['Fields stack', 'Funcs stack', 'Arg name', 'Arg target type', 'Arg value']
+        pretty_table.align = 'l'
+
+        formated_root_field_names_stack = \
+            AliasFuncStackLogFormatter.stackFormat(root_field_names_stack, current_root_field_name)
+        formated_alias_func_stack = AliasFuncStackLogFormatter.stackFormat(alias_func_stack, current_alias_func_name)
+        pretty_table.add_row(
+            [
+                formated_root_field_names_stack,
+                formated_alias_func_stack,
+                self.arg_name,
+                self._arg_type,
+                LogFormatter.formatWarningColor(arg_value)
+            ]
+        )
+
+        print(''.join([
+            f'\n\t{LogFormatter.formatWarning("Invalid json alias func arg value")}',
+            f'\n{str(pretty_table)}'
+        ]))
 
 
 class Alias:
@@ -38,7 +108,15 @@ class Alias:
                 arg_item: ArgItem = item
                 if arg_item.arg_name in alias_func_args:
                     used_arg_names.add(arg_item.arg_name)
-                    texts.append(alias_func_args[arg_item.arg_name])
+                    arg_value_text = alias_func_args[arg_item.arg_name]
+                    converted_art_value = arg_item.convertValue(
+                        arg_value_text,
+                        alias_func_stack,
+                        self.name,
+                        root_field_names_stack,
+                        current_root_field_name
+                    )
+                    texts.append(converted_art_value)
                 else:
                     if missing_arg_names_by_path is None:
                         missing_arg_names_by_path = {}
--- Parser2/PythonScripts/JsonAlias/AliasFactory.py
+++ Parser2/PythonScripts/JsonAlias/AliasFactory.py
@@ -1,5 +1,7 @@
 from typing import Optional
+from prettytable import PrettyTable
 from JsonAlias.Alias import Alias, Item, TextItem, ArgItem
+from Tests import LogFormatter
 
 
 class Pattern:
@@ -9,13 +11,15 @@ class Pattern:
 
 
 def create(name: str, json_alias_func_text: str) -> Alias:
-    items: list[Item] = __Create(json_alias_func_text)
+    items: list[Item] = __Create(name, json_alias_func_text)
 
     return Alias(name, items)
 
 
-def __Create(text: str) -> list[Item]:
+def __Create(name: str, text: str) -> list[Item]:
     result: [Item] = []
+
+    invalid_arg_warnings: list[PrettyTable] = []
 
     # %arg%
     # "%arg%"
@@ -45,7 +49,9 @@ def __Create(text: str) -> list[Item]:
 
             if first_close_pattern is not None and first_close_pattern == current_pattern:
                 result.append(TextItem(text[substring_start_index:char_index]))
-                result.append(ArgItem(text[arg_name_start_index: first_close_pattern_start_char_index]))
+
+                arg_with_type_text = text[arg_name_start_index: first_close_pattern_start_char_index]
+                result.append(_CreateArgItem(arg_with_type_text, invalid_arg_warnings))
 
                 substring_start_index = first_close_pattern_start_char_index + len(current_pattern.close_part)
                 char_index = substring_start_index
@@ -56,6 +62,9 @@ def __Create(text: str) -> list[Item]:
 
     if substring_start_index < text_len:
         result.append(TextItem(text[substring_start_index:]))
+
+    if bool(invalid_arg_warnings):
+        _LogInvalidValue(name, invalid_arg_warnings)
 
     return result
 
@@ -79,3 +88,49 @@ def __IsStartPattern(char_index: int, text: str, open_patter: str) -> bool:
             return False
 
     return True
+
+
+def _CreateArgItem(arg_with_type_text: str, warnings: list[PrettyTable]) -> ArgItem:
+    split_arg = arg_with_type_text.split(':')
+
+    if len(split_arg) != 2:
+        pretty_table = PrettyTable()
+        pretty_table.field_names = ['Description', 'Arg']
+        pretty_table.align = 'l'
+        pretty_table.add_row(["Type is not specified", LogFormatter.formatWarningColor(arg_with_type_text)])
+        warnings.append(pretty_table)
+
+        return ArgItem(arg_with_type_text, ArgItem.ValueType.String)
+
+    arg_name = split_arg[0]
+    arg_type = split_arg[1]
+
+    is_type_valid = \
+        arg_type == ArgItem.ValueType.Bool \
+        or arg_type == ArgItem.ValueType.String \
+        or arg_type == ArgItem.ValueType.Number
+
+    if not is_type_valid:
+        pretty_table = PrettyTable()
+        pretty_table.field_names = ['Description', 'Arg name', 'Arg type']
+        pretty_table.align = 'l'
+        pretty_table.add_row(["Unknown arg type", arg_name, LogFormatter.formatWarningColor(arg_type)])
+        warnings.append(pretty_table)
+        return ArgItem(arg_with_type_text, ArgItem.ValueType.String)
+
+    return ArgItem(arg_name, arg_type)
+
+
+def _LogInvalidValue(
+        alias_func_name,
+        invalid_arg_warnings: list[PrettyTable]
+):
+    pretty_table = PrettyTable()
+    pretty_table.field_names = ['Alias func name', 'Warnings']
+
+    pretty_table.add_row([alias_func_name, '\n\n'.join([str(x) for x in invalid_arg_warnings])])
+
+    print(''.join([
+        f'\n\t{LogFormatter.formatWarning("Invalid json alias func arg")}',
+        f'\n{str(pretty_table)}'
+    ]))
