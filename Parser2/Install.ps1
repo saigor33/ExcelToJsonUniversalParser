5706c24b468266210ac5b83c2e2698d6a9232057
saigor33 <saigor33@yandex.ru>
2025-06-22 13:55:32 +0300

    Bugfix: Fix print step number on install

--- Parser2/Install.ps1
+++ Parser2/Install.ps1
@@ -1,10 +1,20 @@
+Function LogStep {
+	param (
+		[int]$stepNumber,
+		[string]$message
+	)
+	
+	Write-Host "Step ${stepNumber}: ${message}" -foreground green
+}
+
 Function InstallPythonModule {
 	param (
-        [string]$moduleName,
-        [string]$moduleVersions
-    )
-   
-	Write-Host "Step 7: Install python module `"$moduleName`" v.$moduleVersions"
+		[int]$stepNumber,
+		[string]$moduleName,
+		[string]$moduleVersions
+	)
+	
+	LogStep -stepNumber $stepNumber -message "Install python module `"${moduleName}`" v.${moduleVersions}"
 	pip install $moduleName==$moduleVersions
 	Write-Host "Python module `"$moduleName`" installed" -foreground green -nonewline
 	echo `n
@@ -15,6 +25,7 @@ $ErrorActionPreference = "Inquire"
 # Fix execute PowerShell policy
 & "$PSScriptRoot/FixExecutePowerShellPolicy.ps1"
 
+$stepNumber = 1;
 $installationSubScriptsPath = "./InstallationSubScripts/"
 
 if(-not(Test-Path -Path $installationSubScriptsPath))
@@ -23,49 +34,70 @@ if(-not(Test-Path -Path $installationSubScriptsPath))
 }
 
 # Install pyenv
-Write-Host "Step 1: Install penv stage"
+LogStep -stepNumber $stepNumber -message "Install penv"
 $installPyenvFilePath = $installationSubScriptsPath +"install-pyenv-win.ps1"
 Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/pyenv-win/pyenv-win/master/pyenv-win/install-pyenv-win.ps1" -OutFile $installPyenvFilePath
 &$installPyenvFilePath
 write-host "Istall penv finished!" -foreground green -nonewline
 echo `n
 
+$stepNumber++;
+
 # Set Envarament variables
-Write-Host "Step 2: Add Envarament variables"
+LogStep -stepNumber $stepNumber -message "Add Envarament variables"
 & "$PSScriptRoot/SetEnvaramentVariables.ps1"
 
+$stepNumber++;
+
 # Pyenv update cache
-Write-Host "Step 3: Update pyenv cache"
+LogStep -stepNumber $stepNumber -message "Update pyenv cache"
 pyenv update
 Write-Host "Pyenv cache updated" -foreground green -nonewline
 echo `n
 
+$stepNumber++;
+
 # Install Python 3.9.11
-Write-Host "Step 4: Install python 3.9.11"
+LogStep -stepNumber $stepNumber -message "Install python 3.9.11"
 pyenv install 3.9.11
 Write-Host "Python 3.9.11 installed" -foreground green -nonewline
 echo `n
 
+$stepNumber++;
+
 # Select python version in pyenv
-Write-Host "Step 5: Select python version 3.9.11 in pyenv"
+LogStep -stepNumber $stepNumber -message "Select python version 3.9.11 in pyenv"
 pyenv global 3.9.11
 Write-Host "Python version 3.9.11 selected" -foreground green -nonewline
 echo `n
 
+$stepNumber++;
+
 # Install pip
-Write-Host "Step 6: Install pip"
+LogStep -stepNumber $stepNumber -message "Install pip"
 $installPipFilePath = $installationSubScriptsPath +"get-pip.py"
 Invoke-WebRequest -UseBasicParsing -Uri  https://bootstrap.pypa.io/pip/3.7/get-pip.py -OutFile $installPipFilePath
 python $installPipFilePath
 Write-Host "Pip installed!" -foreground green -nonewline
 echo `n
 
-InstallPythonModule -moduleName "numpy" -moduleVersions "1.20.3"
-InstallPythonModule -moduleName "pandas" -moduleVersions "1.5.2"
-InstallPythonModule -moduleName "openpyxl" -moduleVersions "3.1.3"
-InstallPythonModule -moduleName "prettytable" -moduleVersions "3.15.1"
-InstallPythonModule -moduleName "colorama" -moduleVersions "0.4.6"
-InstallPythonModule -moduleName "google-api-python-client" -moduleVersions "2.166.0"
+$stepNumber++;
+
+$pythonModuleVersionsByModuleName = @{
+	"numpy"="1.20.3";
+	"pandas"="1.5.2";
+	"openpyxl"="3.1.3"
+	"prettytable"="3.15.1"
+	"colorama"="0.4.6"
+	"google-api-python-client"="2.166.0"
+}
+
+foreach ($kv in $pythonModuleVersionsByModuleName.GetEnumerator()) {
+	$moduleNmae=$kv.Name;
+	$modulVersion=$kv.Value
+	InstallPythonModule -stepNumber $stepNumber -moduleName $moduleNmae -moduleVersions $modulVersion
+	$stepNumber++;
+}
 
 # Test install
 pyenv version
