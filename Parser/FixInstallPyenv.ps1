ebe5bd21e3091ee2553256105f578579603e3aa8
saigor33 <saigor33@yandex.ru>
2025-07-06 16:01:08 +0300

    Refactor: Remove old parser version

--- Parser/FixInstallPyenv.ps1
+++
@@ -1,9 +0,0 @@
-echo "Step 1: Start fix install pyenv"
-$error.clear()
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
-
-if (!$error) { 
-	Write-Host "Fix install pyenv applied. Rerun Instal.ps1" -foreground green -nonewline
-}
-
-pause
--- Parser/PythonScripts/Configuration/DelimiterPresetConfig.py
+++
@@ -1,4 +0,0 @@
-class DelimiterPresetConfig:
-    def __init__(self, opening_part, closing_part):
-        self.opening_part = opening_part
-        self.closing_part = closing_part
